c504128207ce0cf3269b5f3d6d3a6988ee36618e
nocatchynamehere <wilson.whisnant@gmail.com>
2025-07-05 19:24:31 -0400

    beefed up test cases

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-if __name__ == "__main__":
+def test_get_files_info():
+    # tests for get_files_info
+    print("\n=== Testing get_files_info ===")
     print("Result for current directory:")
     print(get_files_info("calculator", "."))
 
@@ -12,3 +15,22 @@ if __name__ == "__main__":
 
     print("\nResult for '../' directory:")
     print(get_files_info("calculator", "../"))
+
+def test_get_file_content():
+    # tests for get_file_content
+    print("\n=== Testing get_file_content ===")
+    print('\nResult for "Calculator", "lorem.txt"')
+    print(get_file_content("calculator", "lorem.txt"))
+
+    print('\nResult for "calculator", "main.py"')
+    print(get_file_content("calculator", "main.py"))
+
+    print('\nResult for "calculator", "pkg/calculator.py"')
+    print(get_file_content("calculator", "pkg/calculator.py"))
+
+    print('\nResult for "calculator", "/bin/cat"')
+    print(get_file_content("calculator", "/bin/cat"))
+
+if __name__ == "__main__":
+    # test_get_files_info()
+    test_get_file_content()
